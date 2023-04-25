50cc38c42e0096e7e61c06a450df45647d666e2d
Yash Gangwar <gangwaryash06@gmail.com>
2023-04-25 19:53:24 +0530

    fixed module source

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 terraform {
-  
+  cloud {
+    hostname     = "app.terraform.io"
+    organization = "AWS-terraform-7558"
+
+    workspaces {
+      name = "vcs-terraform"
+    }
+  }
 }
 
 provider "aws" {
@@ -7,12 +14,13 @@ provider "aws" {
 }
 
 module "apache" {
-  source           = "./terraform-aws-apache-example"
-  vpc_id           = var.vpc_id
-  my_ip_with_cidr  = var.my_ip_with_cidr
-  public_key       = var.public_key
-  instance_type    = var.instance_type
-  server_name      = var.server_name
+  source          = "yashgangwar7558/apache-example/aws"
+  version         = "1.0.0"
+  vpc_id          = var.vpc_id
+  my_ip_with_cidr = var.my_ip_with_cidr
+  public_key      = var.public_key
+  instance_type   = var.instance_type
+  server_name     = var.server_name
 }
 
 output "public_ip" {
