2e5970ea8e4c02a7f277089b772b67ceb1a0e72a
Yash Gangwar <gangwaryash06@gmail.com>
2023-04-25 19:13:48 +0530

    first commit

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "vpc_id" {
+  type = string
+}
+
+variable "my_ip_with_cidr" {
+  type = string
+  description = "Provide your IP eg. 223.233.73.208/32"
+}
+
+variable "public_key" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+
+}
+
+variable "server_name" {
+  type = string
+
+}
